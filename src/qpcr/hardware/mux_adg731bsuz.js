1ed7e303c04a89dfb0a50fa87ef9fc8b2d31cb37
Mariko Hisakawa <avenafatua@gmail.com>
2020-05-27 16:42:51 +0900

    Implement MUX library

--- src/qpcr/hardware/mux_adg731bsuz.js
+++ src/qpcr/hardware/mux_adg731bsuz.js
@@ -1,13 +1,25 @@
 // 32ch MUX ADG731BSUZ
+const SPI = require('pi-spi');
+const rpio = require('rpio');
+const raspi = require('raspi');
+const pwm = require('raspi-pwm');
+
 class ADG731BSUZ {
-  constructor () {
-    // TODO
+  constructor (spiCh, sync) {
+    this.spi = null;
+    this.spiCh = spiCh;
+    this.sync = sync;
+    rpio.open(this.sync, rpio.OUTPUT, rpio.LOW);
   }
   initialize () {
-    // TODO
+    this.spi = SPI.initialize(this.spiCh);
   }
-  selectChannel () {
-    // TODO
+  selectChannel (channel) {
+    const val = 0xFF & channel;
+    rpio.write(this.sync, rpio.HIGH);
+    this.spi.write(new Buffer([val]), ()=>{
+      rpio.write(this.sync, rpio.LOW);
+    });
     
   }
 }
--- src/qpcr/hardware_test/test_mux_adg731bsuz.js
+++ src/qpcr/hardware_test/test_mux_adg731bsuz.js
@@ -1,9 +1,13 @@
-const TLC59281DBQR = require("../hardware/led_driver_tlc59281dbqr.js");
-let mux = TLC59281DBQR();
+const ADG731BSUZ = require("../hardware/mux_adg731bsuz.js");
+
+
+const SPI_CHANNEL = "/dev/spidev0.0";
+const PIN_SYNC = 40; // Pin number
+let mux = new ADG731BSUZ(SPI_CHANNEL, PIN_SYNC);
 mux.initialize();
 
 let channel = 0;
-const CHANNELS_COUNT = 32;
+const CHANNELS_COUNT = 4;
 setInterval(()=>{
   mux.selectChannel(channel);
   channel = (channel + 1) % CHANNELS_COUNT;
