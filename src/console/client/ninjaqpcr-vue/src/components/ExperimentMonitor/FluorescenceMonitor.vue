d30b86e52eb6e441ddb7191f72a1410790a82ecc
Mariko Hisakawa <avenafatua@gmail.com>
2020-07-20 16:04:23 +0900

    Render threshold lines

--- src/console/client/ninjaqpcr-vue/src/components/ExperimentMonitor/FluorescenceMonitor.vue
+++ src/console/client/ninjaqpcr-vue/src/components/ExperimentMonitor/FluorescenceMonitor.vue
@@ -11,6 +11,12 @@
         style="width:400px,height:200px"
       />
     </div>
+    <ul>
+      <li v-for="(channel, index) in channels"
+        :key="index">
+        {{ channel.label }} / {{ channel.exceeded }} / {{ channel.fluorescence }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -30,10 +36,22 @@ export default {
       oneShot:false,
       continuous:false,
       baselineExists:false,
-      baseline:[]
+      baseline:[],
+      channels:[]
     }
   },
   created: function () {
+    this.channels = [];
+    for (let i=0; i<TUBE_COUNT; i++) {
+      const obj = {
+        label: "Well " + (i+1),
+        fluorescence: 0,
+        threshold: 0,
+        amount: 0,
+        exceeded: false
+      };
+      this.channels.push(obj);
+    }
     device.addTransitionHandler({
       onStart: (obj)=>{
         startTime = new Date();
@@ -41,37 +59,7 @@ export default {
     });
   },
   mounted: function () {
-    const handler = {
-      onFluorescenceUpdate: (data)=>{
-        let timestamp = new Date().getTime() - startTime.getTime();
-        for (let i=0; i<TUBE_COUNT; i++) {
-          graph.addData(i, {t:timestamp, v:data[i]});
-        }
-        const minTime = Math.max(0, timestamp/1000-TIME_RANGE_SEC);
-        graph.setMinMaxX(minTime, minTime + TIME_RANGE_SEC + 10);
-        graph.update();
-      },
-      onFluorescenceEvent: (data)=>{
-        switch (data.type) {
-          case "start":
-          this.continuous = true;
-          break;
-          case "stop": 
-          this.continuous = false;
-          break;
-          case "measure": 
-          this.oneShot = true;
-          setTimeout(()=>{this.oneShot = false}, 1000);
-          break;
-          case "baseline": 
-          this.baselineExists = true;
-          break;
-          default:
-          break;
-        }
-      }
-    };
-    device.addFluorescenceUpdateHandler(handler);
+    device.addFluorescenceUpdateHandler(this);
     device.addBaselineHandler(this);
     
     graph = new Graph('chartFluorescence');
@@ -83,12 +71,56 @@ export default {
     graph.setConversionFunction(
       (obj) =>{return { "x":obj.t/1000.0, "y":obj.v }}
     );
+    this.baseline = device.getBaseline();
+    this.applyBaseline();
     graph.setMinMaxY(0, 1);
   },
   methods: {
+    applyBaseline () {
+      if (this.hasBaseline()) {
+        graph.setHLines(this.baseline.thresholds);
+      }
+    },
+    hasBaseline () {
+      return this.baseline != null && this.baseline.thresholds != null;
+    },
     onBaselineUpdate: function (data) {
-      this.baseline = data.baselines;
-      graph.setHLines(data.thresholds);
+      this.baseline = data;
+      this.applyBaseline();
+    },
+    onFluorescenceUpdate: function (data) {
+      let timestamp = new Date().getTime() - startTime.getTime();
+      for (let i=0; i<TUBE_COUNT; i++) {
+        graph.addData(i, {t:timestamp, v:data[i]});
+        this.channels[i].fluorescence = data[i];
+      }
+      if (this.hasBaseline()) {
+        for (let i=0; i<TUBE_COUNT; i++) {
+          this.channels[i].exceeded = this.channels[i].exceeded || (data[i] > this.baseline.thresholds[i]);
+        }
+      }
+      const minTime = Math.max(0, timestamp/1000-TIME_RANGE_SEC);
+      graph.setMinMaxX(minTime, minTime + TIME_RANGE_SEC + 10);
+      graph.update();
+    },
+    onFluorescenceEvent (data) {
+      switch (data.type) {
+        case "start":
+        this.continuous = true;
+        break;
+        case "stop": 
+        this.continuous = false;
+        break;
+        case "measure": 
+        this.oneShot = true;
+        setTimeout(()=>{this.oneShot = false}, 1000);
+        break;
+        case "baseline": 
+        this.baselineExists = true;
+        break;
+        default:
+        break;
+      }
     }
   }
 }
