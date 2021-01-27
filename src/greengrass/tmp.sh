9e88aed121320965288a0d40840efc701410097f
hisashin <shingohisakawa@gmail.com>
2021-01-27 15:19:32 +0900

    add setup.sh

--- src/greengrass/tmp.sh
+++
@@ -1,4 +0,0 @@
-echo "AWS_ACCESS_KEY_ID=$1"
-echo "AWS_SECRET_ACCESS_KEY=$2"
-echo "JAVA_HOME=$3"
-echo "export AWS_ACCESS_KEY_ID=${1}\nexport AWS_SECRET_ACCESS_KEY=${2}\nexport JAVA_HOME=${3}" >> environment.txt
