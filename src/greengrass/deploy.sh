7bfaa30c8f6c2b323669e098a3d0c14555141fb3
hisashin <shingohisakawa@gmail.com>
2021-02-21 10:00:11 +0900

    fix deploy.sh

--- src/greengrass/deploy.sh
+++ src/greengrass/deploy.sh
@@ -5,8 +5,6 @@ fi
 branch=$1
 echo "Use branch $branch"
 
-// heavy
-//curl -L -o production.zip https://github.com/hisashin/Ninja-qPCR/archive/production.zip
 mkdir sandbox
 cd sandbox
 git init
@@ -14,15 +12,16 @@ git config core.sparsecheckout true
 git remote add origin github:hisashin/Ninja-qPCR.git
 echo src > .git/info/sparse-checkout
 git pull origin $branch
-
 zip -r src-$branch.zip src
-version=`sed -z "s/.*ComponentVersion: *'//" src/greengrass/recipe/create-component-version-$branch.yaml |sed -z "s/'.*//"`
+
+version=`sed -z "s/.*ComponentVersion: *'//" src/greengrass/recipe/$branch.yaml |sed -z "s/'.*//"`
 echo "Upload artifact version $version ($branch)"
 aws s3 cp src-prod.zip s3://gg-ninja-qpcr/artifacts/dev.hisa.Ninja/$version/src-$branch.zip
 echo "Create component version $version ($branch)"
-aws greengrassv2 create-component-version --inline-recipe fileb://Ninja-qPCR-production/src/greengrass/recipe/create-component-version-$branch.yaml
+aws greengrassv2 create-component-version --inline-recipe fileb://src/greengrass/recipe/$branch.yaml
 echo "Revise deployment version $version (prod)"
-sed "s/{version}/$version/" src/greengrass/deployment/revise-$branch.json.template > revise.json
-aws greengrassv2 create-deployment --cli-input-json file://revise.json  
+sed -e "s/{version}/$version/" -e "s/{branch}/$branch/" src/greengrass/revise.json.template > revise.json
+aws greengrassv2 create-deployment --cli-input-json file://revise.json
 cd ..
 rm -rf sandbox
+
