d20b546f8566ae17e6bb47c0f32d1c4e07bfcc1f
hisashin <shingohisakawa@gmail.com>
2021-02-23 06:10:52 +0900

    retry node install

--- src/greengrass/setup.sh
+++ src/greengrass/setup.sh
@@ -5,7 +5,8 @@ sudo apt -y update
 sudo apt -y install openjdk-8-jdk
 sudo adduser --system ggc_user
 sudo addgroup --system ggc_group
-usermod -aG sudo ggc_user
+sudo usermod -aG sudo ggc_user
+sudo sh -c "echo 'ggc_user ALL=NOPASSWD: ALL' >> /etc/sudoers"
 sudo cp /etc/sysctl.d/98-rpi.conf /etc/sysctl.d/98-rpi.conf~
 sudo chmod 774 /etc/sysctl.d/98-rpi.conf
 echo "fs.protected_hardlinks = 1\nfs.protected_symlinks = 1" | sudo tee -a /etc/sysctl.d/98-rpi.conf
