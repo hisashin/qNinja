1d5b3cc727b1ddbefaa92b41bc4adb1aff5521ce
hisashin <shingohisakawa@gmail.com>
2021-02-23 06:01:57 +0900

    add ggc_user to sudo group

--- src/greengrass/setup.sh
+++ src/greengrass/setup.sh
@@ -5,6 +5,7 @@ sudo apt -y update
 sudo apt -y install openjdk-8-jdk
 sudo adduser --system ggc_user
 sudo addgroup --system ggc_group
+usermod -aG sudo ggc_user
 sudo cp /etc/sysctl.d/98-rpi.conf /etc/sysctl.d/98-rpi.conf~
 sudo chmod 774 /etc/sysctl.d/98-rpi.conf
 echo "fs.protected_hardlinks = 1\nfs.protected_symlinks = 1" | sudo tee -a /etc/sysctl.d/98-rpi.conf
