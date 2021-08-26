4dd3f3f367db91351da3ae5413d5f08eb51e48c7
Maripo GODA <avenafatua@gmail.com>
2021-08-26 13:25:18 +0900

    Add optics section class

---
+++ src/qpcr/session_optics.js
@@ -0,0 +1,79 @@
+const eventBus = require("./lib/event_bus.js");
+// Run optics demo with batch3 boards.
+class OpticsSession {
+  constructor () {
+    eventBus.subscribe("optics.command", (topic, data)=>{
+      // TODO
+    });
+    this.measurement = [];
+    for (let i=0; i<16; i++) {
+      this.measurement.push(
+        {
+          id:i,
+          vl:0,// Small gain
+          vs:0// Small gain
+        }
+      );
+    }
+  }
+  setHardware (thermalCycler, optics) {
+    this.optics = optics;
+    /*
+    if (!USE_DUMMY) {
+      rpio.open(32, rpio.INPUT); // To disable PWM pin for ADA2200's RCLK
+    }
+    */
+  }
+  transform (values) {
+    for (let i=0; i<16; i++) {
+      let v = values[0][i];
+      if (v.s=="10M") {
+        this.measurement[i].vl = v.v;
+      } else {
+        this.measurement[i].vs = v.v;
+      }
+    }
+    return this.measurement;
+  }
+  runOpticsDemo () {
+    console.log("runOpticsDemo");
+    this.optics.start();
+    this.opticsStarted = true;
+    setInterval(()=>{
+      this.optics.measureAll((values)=>{
+        values.forEach((ch, chId)=>{
+          ch.forEach((well, wellId)=>{
+            well.id = wellId;
+          });
+        });
+        eventBus.publish("optics.update.measurement", this.transform(values));
+      });
+    }, 3000);
+  }
+  shutdown () {
+    console.log("Shutting down demo_optics_batch8");
+    if (this.opticsStarted) {
+      try {
+        this.optics.shutdown();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    if (this.ledStarted) {
+      try {
+        this.led.shutdown();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    if (this.photosensingStarted) {
+      try {
+        this.photosensing.shutdown();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+  }
+}
+
+module.exports = OpticsSession;
